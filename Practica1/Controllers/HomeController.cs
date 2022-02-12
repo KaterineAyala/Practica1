2e2310fb98c264110b68c13fd7a01e95ed07c384
Katerine Idalia Ayala Peraza <ap0419032019@unab.edu.sv>
2022-02-11 19:33:20 -0600

    Agregar archivos de proyecto.

---
+++ Practica1/Controllers/HomeController.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Practica1.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practica1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        //Importante sin una accion no tenemos vistas
+        public IActionResult Suma(Double DatoN1, Double DatoN2)
+        {
+            if (DatoN1 > 10 || DatoN2 > 10)
+            {
+                return View("NoMayores");
+            }
+
+            Double resultado = DatoN1 + DatoN2;
+
+            ViewBag.DatoN1V = DatoN1;
+            ViewBag.DatoN2V = DatoN2;
+            ViewBag.VariableAEnviar = resultado;
+
+            return View();
+        }
+
+        public IActionResult NoMayores()
+        {
+            return View();
+        }
+
+        public IActionResult Resta(Double DatoN1, Double DatoN2)
+        {
+            if (DatoN1 > 10 || DatoN2 > 10)
+            {
+                return View("NoMayores");
+            }
+
+            Double resultado = DatoN1 - DatoN2;
+
+            ViewBag.DatoN1V = DatoN1;
+            ViewBag.DatoN2V = DatoN2;
+            ViewBag.VariableAEnviar = resultado;
+
+            return View();
+        }
+
+        public IActionResult Multiplicacion(Double DatoN1, Double DatoN2)
+        {
+            if (DatoN1 > 10 || DatoN2 > 10)
+            {
+                return View("NoMayores");
+            }
+
+            Double resultado = DatoN1 * DatoN2;
+
+            ViewBag.DatoN1V = DatoN1;
+            ViewBag.DatoN2V = DatoN2;
+            ViewBag.VariableAEnviar = resultado;
+
+            return View();
+        }
+
+        public IActionResult Division(Double DatoN1, Double DatoN2)
+        {
+            if (DatoN1 > 10 || DatoN2 > 10)
+            {
+                return View("NoMayores");
+            }
+
+            Double resultado = DatoN1 / DatoN2;
+
+            ViewBag.DatoN1V = DatoN1;
+            ViewBag.DatoN2V = DatoN2;
+            ViewBag.VariableAEnviar = resultado;
+
+            return View();
+        }
+
+    }
+}
